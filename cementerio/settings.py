002bde536f850a981e4e7fc87f0b2a7be45c346d
UrieL0x <143738012+UriL0x@users.noreply.github.com>
2025-06-01 13:06:58 -0700

    wa1

--- cementerio/settings.py
+++ cementerio/settings.py
@@ -130,9 +130,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
+#CORS_ALLOWED_ORIGINS = [
+ #   'http://localhost:3000'
+#S]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
